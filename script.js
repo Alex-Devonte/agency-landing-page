d8553c350b03d7f466214786e1ec8ae8ebbb4568
Alex <alex95.taylor@gmail.com>
2025-08-03 16:40:17 -0400

    Add logic for opening/closing of hamburger menu

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggleBtn = document.querySelector("[data-menu-toggle]");
+  const menu = document.querySelector("[data-menu]");
+
+  function toggleMenu() {
+    const isExpanded = menuToggleBtn.getAttribute("aria-expanded") === "true";
+    menu.classList.toggle("menu-open");
+    menuToggleBtn.setAttribute("aria-expanded", !isExpanded);
+    menuToggleBtn.setAttribute(
+      "aria-label",
+      isExpanded ? "Open menu" : "Close menu",
+    );
+
+    menuToggleBtn.classList.toggle("menu-active", !isExpanded);
+  }
+
+  document
+    .querySelectorAll("[data-menu] a,  [data-menu] button")
+    .forEach((link) => {
+      link.addEventListener("click", () => {
+        //Close menu when link or button is clicked
+        menu.classList.remove("menu-open");
+        menuToggleBtn.setAttribute("aria-expanded", "false");
+        menuToggleBtn.setAttribute("aria-label", "Open menu");
+        menuToggleBtn.classList.remove("menu-active");
+      });
+    });
+
+  menuToggleBtn.addEventListener("click", toggleMenu);
+});
